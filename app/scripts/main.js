f9d4a057aa34be858079e1455b9dee4b9794f0d7
Seth Corker <seth.corker@gmail.com>
2016-03-31 02:48:04 +1300

    Working, haml compiled then formatted to remove = after <--! in HTML comment, should be fixed to maintain

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('Heard about the good news?');
+$(window).load(function() {
+
+    $("#oriental-bay").twentytwenty();
+
+});
+window.sr = ScrollReveal();
+
+sr.reveal('.panel', {
+    delay: 200
+});
